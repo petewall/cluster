c4b02f44a659e7963acbf50113af8ceda7f4b3a4
Pete Wall <pete@petewall.net>
2022-04-13 21:44:03 -0500

    Convert cert-manager to kapp
    Signed-off-by: Pete Wall <pete@petewall.net>

--- deployments/cert-manager/deploy.sh
+++ deployments/cert-manager/deploy.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.7.1/cert-manager.yaml
+kapp deploy -a cert-manager -f https://github.com/cert-manager/cert-manager/releases/download/v1.7.1/cert-manager.yaml
+kapp deploy -a cert-manager-issuers -f issuer.yaml
