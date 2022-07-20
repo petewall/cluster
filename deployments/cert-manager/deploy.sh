a05a39ed432a9bd7c59e7262cac1adab6aa99118
Pete Wall <pete@petewall.net>
2022-07-19 22:13:37 -0500

    Show diffs when deploying
    Signed-off-by: Pete Wall <pete@petewall.net>

--- deployments/cert-manager/deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-kapp deploy -a cert-manager -f https://github.com/cert-manager/cert-manager/releases/download/v1.7.1/cert-manager.yaml
-kapp deploy -a cert-manager-issuers -f issuer.yaml
